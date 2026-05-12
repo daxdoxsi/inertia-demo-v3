06e50f785ad97364d3066e6f57b9ef8de218f13e
David M. Miller <daxdoxsi@gmail.com>
2026-05-12 13:41:18 -0600

    Fix the route to the controller's methods of the class UserController.
    Signed-off-by: David M. Miller <daxdoxsi@gmail.com>

--- app/Http/Controllers/Dmm/UserController.php
+++ app/Http/Controllers/Dmm/UserController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers\Dmm;
 
 use App\Models\User;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class UserController
 {
-    public function show(User $user): \Inertia\Response
+    public function show(User $user): Response
     {
         return Inertia::render('User/Show', [
-            'user' => $user
+            'user' => $user,
         ]);
     }
 }
